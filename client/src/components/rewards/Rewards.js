bf9838c4301ca1e9c97c3843c3a5d41b01fa217a
chris bodin <xtropicalmuffin@gmail.com>
2022-01-13 20:31:44 -0700

    Rewards views

--- client/src/components/rewards/Rewards.js
+++ client/src/components/rewards/Rewards.js
@@ -1,17 +1,29 @@
 import { useEffect, useState } from 'react';
-import { RewardConsumer } from '../../providers/RewardProvider';
+import { RewardConsumer } from '../../rewards/RewardProvider';
+import RewardList from './RewardList';
+import { Button } from 'react-bootstrap';
+import RewardForm from './RewardForm';
 
-const Rewards = ({ getAllRewards }) => {
-  
-  // useEffect( () => {
-  //   getAllRewards()
-  // }, [])
+const Rewards = ({ rewards, getAllRewards, addReward }) => {
+  const [adding, setAdding] = useState(false)
+
+  useEffect( () => {
+    getAllRewards()
+  }, [])
 
   return (
     <>
-    <h1>All Rewards</h1>
+      <h1>All Rewards</h1>
+      { adding ?
+          <>
+            <RewardForm addReward={addReward} />
+            <Button variant="info" onClick={() => setAdding(false)}>Cancel</Button>
+          </>
+        :
+        <Button variant="info" onClick={() => setAdding(true)}>+</Button>
+      }
+      <RewardList rewards={Rewards} />
     </>
-
   )
 }
 
