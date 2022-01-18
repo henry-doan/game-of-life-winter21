838e6967d37105917c5443abbe3bb3c0ece41dbf
steph medlin <stephanieanndavis13@gmail.com>
2022-01-17 18:46:43 -0700

    migration to change task model schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_025212) do
+ActiveRecord::Schema.define(version: 2022_01_18_013716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2022_01_13_025212) do
   end
 
   create_table "tasks", force: :cascade do |t|
-    t.string "task"
+    t.string "title"
     t.datetime "due"
     t.text "comment"
     t.string "image"
@@ -36,6 +36,10 @@ ActiveRecord::Schema.define(version: 2022_01_13_025212) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "add_sub"
+    t.string "diff_levels"
+    t.string "tags"
+    t.string "frequency"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
